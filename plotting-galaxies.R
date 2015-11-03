e4d22ea829438e473c22b53bd68b36449f1f5b06
Jacob Bieker <jacob@bieker.tech>
2015-11-03 14:01:22 -0800

    Initial Commit

---
+++ plotting-galaxies.R
@@ -0,0 +1,32 @@
+#########################################################
+#
+#         Install the needed dependancies
+#
+#########################################################
+if(require("XLConnect")){
+  print("XLConnect is loaded correctly")
+} else {
+  print("trying to install XLConnect")
+  install.packages("XLConnect")
+  if(require("XLConnect")){
+    print("XLConnect are installed and loaded")
+  } else {
+    stop("could not install XLConnect, yaml, gdata")
+  }
+}
+
+#########################################################
+#
+#       Loading the data
+#
+#########################################################
+
+spectrophotometric_data_workbook <- loadWorkbook("GCP_spectrophot_data.xlsx")
+
+spectrophotometric_data <- readWorksheet(spectrophotometric_data_workbook, sheet = getSheets(spectrophotometric_data_workbook))
+
+#########################################################
+#
+#    Functions to calculate the necessary values
+#
+#########################################################
