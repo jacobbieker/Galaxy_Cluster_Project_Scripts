0db249b021d4ea8d2d530d4d94920e7af701820a
Jacob Bieker <jacob@bieker.tech>
2015-11-04 12:48:29 -0800

    Start adding the ggplot2 data

--- plotting-galaxies.R
+++ plotting-galaxies.R
@@ -3,15 +3,17 @@
 #         Install the needed dependancies
 #
 #########################################################
-if(require("XLConnect")){
-  print("XLConnect is loaded correctly")
+if(require("XLConnect") && require("ggplot2")){
+  print("XLConnect and ggplot2 are loaded correctly")
 } else {
-  print("trying to install XLConnect")
+  print("Trying to install XLConnect")
   install.packages("XLConnect")
-  if(require("XLConnect")){
+  print("Trying to install ggplot2")
+  install.packages("ggplot2")
+  if(require("XLConnect") && require("ggplot2")){
     print("XLConnect are installed and loaded")
   } else {
-    stop("could not install XLConnect, yaml, gdata")
+    stop("could not install XLConnect, ggplot2")
   }
 }
 
@@ -36,4 +38,41 @@ get.quad.error <- function(error.1, error.2) {
   error <- sqrt(error.1^2 + error.2^2)
   return(error);
 }
+
+#########################################################
+#
+#   Functions to graph the data
+#
+#########################################################
+
+# Returns the scatter points of log(sigma) vs log(Mass) to plot with ggplot2
+graph.sigma.mass <- function(sheet, colorcode, shapecode) {
+  re.data <- sheet$LREJB_KPC_DEV
+  re.error <- sheet$E_LRE_DEVAF814W
+  sigma.data <- sheet$
+}
+
+# Returns the scatter points of log(re) vs log(Mass) to plot with ggplot2
+graph.re.mass <- function(sheet, colorcode, shapecode) {
+  re.data <- sheet$LREJB_KPC_DEV
+  re.error <- sheet$E_LRE_DEVAF814W
+  mass.data <- sheet$LMASS_DEV
+  mass.error <- sheet$E_LMASS_DEV
+  return(geom_point())
+}
+
+# Returns the scatter points of 1.30log(sigma) - 0.82log(<I>e) vs log(re) to plot with ggplot2
+graph.sigmaI.re <- function(sheet, res, colorcode, shapecode) {
+  re.data <- sheet$LREJB_KPC_DEV
+  re.error <- sheet$E_LRE_DEVAF814W
+  ie.data <- sheet$LIEJB_DEV
+  ie.error <- sheet$e_lieJB_DEV # 
+  sigma.data <- sheet$LSIGMA_COR # log10(sigma) in km/s
+  sigma.error <- sheet$E_LSIGMA # Error in Sigma
+}
+#########################################################
+#
+#   Go through whichever sheets are needed and graph
+#
+##########################################################
 
