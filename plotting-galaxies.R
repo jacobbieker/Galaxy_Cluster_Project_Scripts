c492c63d007761cfafffed220ec0781a8a5e5de0
Jacob Bieker <jacob@bieker.tech>
2016-02-22 13:47:56 -0800

    Try adding tick marks on all sides
    Taken from https://stackoverflow.com/questions/28949001/mirroring-axis-ticks-in-ggplot2/29023682#29023682

--- plotting-galaxies.R
+++ plotting-galaxies.R
@@ -3,15 +3,15 @@
 #         Install the needed dependancies
 #
 #########################################################
-if(require("XLConnect") && require("ggplot2") && require("scatterplot3d")){
+if(require("XLConnect") && require("ggplot2") && require(gtable) && require("scatterplot3d")){
   print("XLConnect and ggplot2 are loaded correctly")
 } else {
   print("Trying to install XLConnect")
   install.packages("XLConnect", dependencies = TRUE)
   print("Trying to install ggplot2")
-  install.packages("ggplot2")
+  install.packages("ggplot2", dependencies = TRUE)
   print("Trying to install scatterplot3d")
-  install.packages("scatterplot3d")
+  install.packages("scatterplot3d", dependencies = TRUE)
   if(require("XLConnect") && require("ggplot2") && require("scatterplot3d")){
     print("XLConnect, ggplot2, and scatterplot3d are installed and loaded")
   } else {
@@ -186,6 +186,60 @@ lsigma.vs.logml <- ggplot() + theme_bw() +
   # Currently calculated by coef(lm(data=coma.data, lML_JB_DEV ~ lsigma_cor))
   geom_abline(intercept = -0.8569, slope=0.7535)
   
+################################
+# Adding tick marks on all sides
+################################
+# Convert the plot to a grob
+gt <- ggplotGrob(lsigma.vs.logml)
+
+# Get the position of the panel in the layout
+panel <-c(subset(gt$layout, name=="panel", se=t:r))
+
+## For the bottom axis
+# Get the row number of the bottom axis in the layout
+rn <- which(gt$layout$name == "axis-b")
+
+# Extract the axis (tick marks only)
+axis.grob <- gt$grobs[[rn]]
+axisb <- axis.grob$children[[2]]  # Two children - get the second
+axisb  # Note: two grobs - tick marks and text
+
+# Get the tick marks
+xaxis = axisb$grobs[[1]]  # NOTE: tick marks first
+xaxis$y = xaxis$y - unit(0.25, "cm")  # Position them inside the panel
+
+# Add a new row to gt, and insert the revised xaxis grob into the new row.
+gt <- gtable_add_rows(gt, unit(0, "lines"), panel$t-1)
+gt <- gtable_add_grob(gt, xaxis, l = panel$l, t = panel$t, r = panel$r, name = "ticks")
+
+## Repeat for the left axis
+# Get the row number of the left axis in the layout
+panel <-c(subset(gt$layout, name=="panel", se=t:r))
+rn <- which(gt$layout$name == "axis-l")
+
+# Extract the axis (tick marks and axis text)
+axis.grob <- gt$grobs[[rn]]
+axisl <- axis.grob$children[[2]]  # Two children - get the second
+axisl  # Note: two grobs -  text and tick marks
+
+# Get the tick marks
+yaxis = axisl$grobs[[2]] # NOTE: tick marks second
+yaxis$x = yaxis$x - unit(0.25, "cm") # Position them inside the panel
+
+# Add a new column to gt, and insert the revised yaxis grob into the new column.
+gt <- gtable_add_cols(gt, unit(0, "lines"), panel$r)
+gt <- gtable_add_grob(gt, yaxis, t = panel$t, l = panel$r+1, name = "ticks")
+
+# Turn clipping off
+gt$layout[gt$layout$name == "ticks", ]$clip = "off"
+
+# Draw it
+grid.draw(gt)
+
+#######################
+# End adding tick marks
+######################
+
 lsigma.vs.logml
 
 library(reshape2)
